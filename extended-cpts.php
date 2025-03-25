0e29e709ed904335e3a72cb2c6cc937e13c7afc8
David Kemp <2870080+x2764tech@users.noreply.github.com>
2025-03-25 12:04:46 +0000

    feature: base

---
+++ extended-cpts.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Plugin Name: Extended-CPTS
+ * Version:
+	Plugin URI: https://github.com/x2764tech/extended-cpts
+    Update Uri: https://github.com/x2764tech/extended-cpts
+	Description:Wrapper around extended-cpts library
+	Version: 0.0.1
+	Author: x2764tech limited
+    Author URI: https://x2764te.ch/
+	Copyright 2025 Shared and distributed by x2764tech limited
+ */
+
+
+require_once 'vendor/autoload.php';
+
+$x2764tech_xcpts__updater = new Inc2734\WP_GitHub_Plugin_Updater\Bootstrap(
+    plugin_basename( __FILE__ ),
+    'x2764tech-limited',
+    'extended-cpts'
+);
